56961320b2df89461f8ba12d298c9c590cc68306
Fernando Doglio <deleteman@gmail.com>
2023-04-27 23:11:21 +0200

    adding visits modules from vercel

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,15 @@ import '@/styles/globals.css'
 import '../styles/bootstrap.scss'
 import MainLayout from '../layouts/MainLayout'
 import { UserProvider } from '@/components/UserProvider'
+import{ Analytics } from '@vercel/analytics/react';
+
 import 'react-tooltip/dist/react-tooltip.css'
 
 export default function App({ Component, pageProps }) {
   return <UserProvider>
     <MainLayout>
       <Component {...pageProps} />
+       <Analytics/>
 </MainLayout>
   </UserProvider>
 }
